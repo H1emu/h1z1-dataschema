ca6a267a575b1790f5ec6888b954b776de8bdd31
Quentin Gruber <quentingruber@gmail.com>
2021-09-29 00:05:10 +0200

    create some bases for unit tests

---
+++ tests/main.test.ts
@@ -0,0 +1,7 @@
+import dataschema from "../dataschema";
+
+import { testPack } from "./pack.test";
+
+testPack();
+
+console.log("tests done")
---
+++ tests/pack.test.ts
@@ -0,0 +1,35 @@
+import dataschema from "../dataschema";
+const { pack } = dataschema;
+
+export function testPack() {
+    testSkyChangePacket();
+    testRespawnLocationsPacket();
+}
+
+function testSkyChangePacket() {
+    const schema = require("./data/skychangeschema.json");
+
+    const obj = require("./data/skychangeobj.json");
+
+    const expected =  Buffer.from(require("./data/skychangeresult.json"));
+
+    const result = pack(schema,obj)
+
+    if(result.data.compare(expected) !== 0){
+        throw new Error("SkyChange pack failed")
+    }
+}
+
+function testRespawnLocationsPacket() {
+    const schema = require("./data/RespawnLocationsschema.json");
+
+    const obj = require("./data/RespawnLocationsobj.json");
+
+    const expected =  Buffer.from(require("./data/RespawnLocationsresult.json"));
+
+    const result = pack(schema,obj)
+
+    if(result.data.compare(expected) !== 0){
+        throw new Error("RespawnLocations pack failed")
+    }
+}
