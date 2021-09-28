fc63619b3ad1451133cbc16959848ab78f19e25a
Quentin Gruber <quentingruber@gmail.com>
2021-09-29 00:41:12 +0200

    npm test

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,8 @@
-import dataschema from "../dataschema";
-
 import { testPack } from "./pack.test";
 
+import { testParse } from "./parse.test";
+
 testPack();
+testParse();
 
 console.log("tests done")
