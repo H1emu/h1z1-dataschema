3ada444e32280585977cff1474dd1656d153da76
Quentin Gruber <quentingruber@gmail.com>
2022-02-26 23:31:12 +0100

    updt h1z1Buffer interface

--- src/dataschema.ts
+++ src/dataschema.ts
@@ -3,9 +3,13 @@ import "h1z1-buffer";
 export interface h1z1Buffer extends Buffer {
   writeBytes(value: any, offset: number, length?: any): any;
   writePrefixedStringLE(value: any, offset: number): any;
+  writeUInt64String(value: any, offset: number): any;
+  writeInt64String(value: any, offset: number): any;
   writeNullTerminatedString(value: any, offset: number): any;
   readBytes(offset: number, length: any): any;
   readPrefixedStringLE(offset: number): any;
+  readUInt64String(offset: number): any;
+  readInt64String(offset: number): any;
   readNullTerminatedString(offset: number): any;
 }
 
