d3c08776256a71865bbf8dfe3025440dbbabb38f
Quentin Gruber <quentingruber@gmail.com>
2023-04-12 02:27:12 +0200

    Revert "add time log"
    This reverts commit ad274ec6d3ecfab9a778d3a96f260b3529c75c69.

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,11 +2,7 @@ import { testPack } from "./pack.test";
 
 import { testParse } from "./parse.test";
 
-console.time("pack")
 testPack();
-console.timeEnd("pack")
-console.time("parse")
 testParse();
-console.timeEnd("parse")
 
 console.log("tests done")
