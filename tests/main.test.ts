ad274ec6d3ecfab9a778d3a96f260b3529c75c69
Quentin Gruber <quentingruber@gmail.com>
2023-04-12 02:21:05 +0200

    add time log

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,7 +2,11 @@ import { testPack } from "./pack.test";
 
 import { testParse } from "./parse.test";
 
+console.time("pack")
 testPack();
+console.timeEnd("pack")
+console.time("parse")
 testParse();
+console.timeEnd("parse")
 
 console.log("tests done")
