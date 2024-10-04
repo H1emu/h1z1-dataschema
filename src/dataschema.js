4279b14cdd21be28019bae8305506463e15a470f
quentingruber <quentingruber@gmail.com>
2024-10-04 16:24:46 +0200

    forgot to compile :)

--- src/dataschema.js
+++ src/dataschema.js
@@ -589,6 +589,9 @@ function pack(fields, object, dataToPack, offset) {
                 customData.copy(data, offset);
                 offset += customData.length;
                 break;
+            case "debug":
+                console.log(field.name);
+                break;
             default:
                 throw `Unknown field type: ${field.type}`;
         }
